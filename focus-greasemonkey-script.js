763bd9f2cd22ea04cdccc108da684662e1edc635
Jon Skulski <jskulski@gmail.com>
2015-05-21 17:55:57 -0700

    Regenerate script. Ignore

--- focus-greasemonkey-script.js
+++ focus-greasemonkey-script.js
@@ -1,17 +1,16 @@
 // ==UserScript==
-// @name        focus
+// @name        Focus
 // @namespace   JSK
-// @description Block distractions and show beautiful stuff
-// @include     about:addons
-// @include     https://facebook.com/*
-// @include     http://reddit.com/*
-// @include     http://twitter.com/*
+// @description Block distractions
+// @include     https://www.facebook.com/*
+// @include     http://www.twitter.com/*
+// @include     http://www.reddit.com/*
+// @include     http://www.imgur.com/*
 // @include     http://inbox.google.com/*
-// @include     http://gmail.com/*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
-var quote = '<h1>&ldquo;Without hard work, nothing grows but weeds.&rdquo;</h1> - Gordon B. Hinckley'
+var quote = '<h1>&ldquo;Without hard work, nothing grows but weeds.&rdquo;</h1> - Gordon B. Hinck
 var body = document.getElementsByTagName('body')[0];
 body.innerHTML = quote;
